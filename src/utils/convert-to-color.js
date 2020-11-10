4fe62a9ffe1d520f3b3a387782a14ca6ba2a9973
spacehaz2 <spacehaz@gmail.com>
2020-11-10 18:40:43 +0300

    added fix

--- src/utils/convert-to-color.js
+++ src/utils/convert-to-color.js
@@ -1,9 +1,9 @@
 export default (str) => {
-    var sessionStoreKey = "myStringColors" + str;
-    if (!sessionStorage[sessionStoreKey ]) {
-        sessionStorage[sessionStoreKey] = Math.random()*0xFFFFFF<<0;       
-    }
-    var randomColor = sessionStorage[sessionStoreKey];
+  var hash = 0;
+  for(var i=0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 3) - hash);
+  }
+  var color = Math.abs(hash).toString(16).substring(0, 6);
 
-    return '#'+ randomColor;
+  return "#" + '000000'.substring(0, 6 - color.length) + color;
 }
